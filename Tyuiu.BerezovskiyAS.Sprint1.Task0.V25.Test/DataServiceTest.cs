f11d04bdf3608e55556fc5d9a6e487d0aa17b6c3
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-10-03 02:18:04 +0500

    Add project files.

---
+++ Tyuiu.BerezovskiyAS.Sprint1.Task0.V25.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint1.Task0.V25.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint1.Task0.V25.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint1.Task0.V25/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint1.Task0.V25.Lib;
+
+{
+    DataService ds = new DataService();
+
+    Console.Title = "Спринт #1 | Выполнил: Березовский А. C. | ПИНб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #1                                                               *");
+    Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+    Console.WriteLine("* Задание #0                                                              *");
+    Console.WriteLine("* Вариант #25                                                             *");
+    Console.WriteLine("* Выполнил: Березовский Артём Станиславович | ПИНб-25-1                   *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Написать программу, которая вычисляет выражение (30 / 6 - 4) * 3        *");
+    Console.WriteLine("* и печатает результат на экране.                                         *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* (30 / 6 - 4) * 3                                                        *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine(ds.Calculate());
+    Console.ReadLine();
+}
+
+
