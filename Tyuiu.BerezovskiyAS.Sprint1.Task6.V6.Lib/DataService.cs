a3c4c5e8e99a356c6b6cb887781a6935390bfe58
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-10-12 23:10:20 +0500

    Task6

---
+++ Tyuiu.BerezovskiyAS.Sprint1.Task6.V6.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BerezovskiyAS.Sprint1.Task6.V6.Lib
+{
+    public class DataService : ISprint1Task6V6
+    {
+        public string DeleteFirstLetter(string value)
+        {
+            string[] words = value.Split(' ');
+            string result = "";
+
+            foreach (string i in words)
+            {
+                if (i.Length > 1)
+                {
+                    string temp = i.Substring(1);
+
+                    result += temp + " ";
+                }
+
+                //else if  (i.Length == 1) {
+                //   result += i + " ";
+                //}
+
+            }
+
+
+            return result.Trim();
+        }
+
+    }
+    
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint1.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint1.Task6.V6.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint1.Task6.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            DataService ds = new DataService();
+
+            string value = "Hello World";
+            string wait = "ello orld";
+            var res = ds.DeleteFirstLetter(value);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
