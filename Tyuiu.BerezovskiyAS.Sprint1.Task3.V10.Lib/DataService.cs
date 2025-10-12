938f90b6ab2bc2afdf2eff4ec2d7c0af848d319b
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-10-12 16:02:43 +0500

    Task3

--- Tyuiu.BerezovskiyAS.Sprint1.Task3.V10.Lib/DataService.cs
+++ Tyuiu.BerezovskiyAS.Sprint1.Task3.V10.Lib/DataService.cs
@@ -3,9 +3,9 @@ namespace Tyuiu.BerezovskiyAS.Sprint1.Task3.V10.Lib
 {
     public class DataService : ISprint1Task3V10
     {
-        public double NumberToMoney(double number)
+        public string NumberToMoney(double number)
         {
-            return number;
+            return number.ToString();
         }
     }
 }
