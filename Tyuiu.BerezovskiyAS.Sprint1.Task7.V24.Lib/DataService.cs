9638aebd5e1734882187323fc6ac7fefc9714980
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-10-13 20:25:25 +0500

    Task7

---
+++ Tyuiu.BerezovskiyAS.Sprint1.Task7.V24.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BerezovskiyAS.Sprint1.Task7.V24.Lib
+{
+    public class DataService : ISprint1Task7V24
+    {
+        public double Calculate(double x, double y)
+        {
+            double res = Math.Round((1 + ((Math.Cos(Math.Sqrt(x + 1))))) / (Math.Sin(15 * y - 4)), 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint1.Task7.V24.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint1.Task7.V24.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint1.Task7.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 8;
+            double wait = 1.617;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
